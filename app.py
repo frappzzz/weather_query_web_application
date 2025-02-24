23e3b7238404d96a04dc002801c5d9e761ccbbe8
frappzzz <frappz06@gmail.com>
2025-02-24 19:49:01 +0300

    Some changes in app

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ app = Flask(__name__, static_folder=config.APP_STATIC_FOLDER, template_folder=co
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method=="GET":
-        weather_data=get_weather_data("Minsk")
-        print(weather_data["weather"][0]['main'], weather_data["weather"][0]['description'], weather_data["main"]['temp'],weather_data["main"]['feels_like'], weather_data["wind"]["speed"],wind_direction(weather_data["wind"]["deg"]),timestamp_to_hms_format(weather_data["sys"]["sunrise"]),timestamp_to_hms_format(weather_data["sys"]["sunset"]))
-        return weather_data
+    if request.method=="POST":
+        city_name=request.form['city']
+        weather_data=get_weather_data(city_name)
+        if weather_data:
+            print(weather_data["sys"]["sunrise"],weather_data["sys"]["sunset"])
+            print(weather_data["weather"][0]['main'], weather_data["weather"][0]['description'], weather_data["main"]['temp'],weather_data["main"]['feels_like'], weather_data["wind"]["speed"],wind_direction(weather_data["wind"]["deg"]),timestamp_to_hms_format(weather_data["sys"]["sunrise"]),timestamp_to_hms_format(weather_data["sys"]["sunset"]))
+            save_weather_query(weather_data["name"],weather_data["weather"][0]['main'], weather_data["weather"][0]['description'], weather_data["main"]['temp'],weather_data["main"]['feels_like'], weather_data["wind"]["speed"],wind_direction(weather_data["wind"]["deg"]),timestamp_to_hms_format(weather_data["sys"]["sunrise"]),timestamp_to_hms_format(weather_data["sys"]["sunset"]))
+        return redirect(url_for('index'))
+    return render_template('index.html')
 @app.route('/history')
 def history():
     history=get_weather_history()
