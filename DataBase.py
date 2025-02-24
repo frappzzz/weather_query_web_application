5d8060396fe55ef09b387a64d6e6e3ed8ddfa9f2
frappzzz <frappz06@gmail.com>
2025-02-24 19:17:50 +0300

    Changed DataBase file

--- DataBase.py
+++ DataBase.py
@@ -6,13 +6,13 @@ import config
 def get_DB_connection():
     conn = psycopg2.connect(dbname=config.DB_NAME, user=config.DB_USER, password=config.DB_PASSWORD, host=config.DB_HOST, port=config.DB_PORT)
     return conn
-def save_weather_query(city_name, weather_main, weather_description,temperature, wind_speed, wind_deg, sunrise,sunset):
+def save_weather_query(city_name, weather_main, weather_description,temperature, temperature_feels_like,wind_speed, wind_deg, sunrise,sunset):
     conn = get_DB_connection()
     cur = conn.cursor()
     cur.execute(
-        sql.SQL("INSERT INTO weather_queries (city_name, weather_main, weather_description,temperature, wind_speed, wind_deg, sunrise,sunset) "
-                "VALUES (%s, %s, %s,%s, %s, %s,%s, %s)"),
-        (city_name, weather_main, weather_description,temperature, wind_speed, wind_deg, sunrise,sunset)
+        sql.SQL("INSERT INTO weather_queries (city_name, weather_main, weather_description,temperature, temperature_feels_like, wind_speed, wind_deg, sunrise,sunset) "
+                "VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s)"),
+        (city_name, weather_main, weather_description,temperature, temperature_feels_like, wind_speed, wind_deg, sunrise,sunset)
     )
     conn.commit()
     cur.close()
@@ -21,7 +21,7 @@ def save_weather_query(city_name, weather_main, weather_description,temperature,
 def get_weather_history():
     conn = get_DB_connection()
     cur = conn.cursor()
-    cur.execute("SELECT query_timestamp, city_name, weather_main, weather_description,temperature, wind_speed, wind_deg, sunrise,sunset FROM weather_queries ORDER BY query_timestamp DESC")
+    cur.execute("SELECT query_timestamp, city_name, weather_main, weather_description,temperature, temperature_feels_like, wind_speed, wind_deg, sunrise,sunset FROM weather_queries ORDER BY query_timestamp DESC")
     history = cur.fetchall()
     cur.close()
     conn.close()
